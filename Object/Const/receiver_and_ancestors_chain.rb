7cabc0295165242f6f3d09fbd726b6c15acfaaac
yym-yumeng123 <18026493873@163.com>
2020-08-12 23:10:22 +0800

    类和模块 里的 方法查找 ancestors

---
+++ Object/Const/receiver_and_ancestors_chain.rb
@@ -0,0 +1,22 @@
+# 消息 接收者和祖先lm
+# 接收者 - my_string.reverse() 中的 my_string 就是接收者
+# 祖先链 - 从一个类 经过它的超类...... 一直到达 BasicObject 类, 这个路径就是该类的组限量
+
+
+# 方法查找: 顺着祖先链向上查找
+
+
+class MyClass
+  def my_method
+    'my_method()'
+  end
+end
+
+class MySubClass < MyClass
+
+end
+
+obj = MySubClass.new
+p obj.my_method  # 方法查找 看图 ./方法查找.png
+
+p MySubClass.ancestors  # [MySubClass, MyClass, Object, Kernel, BasicObject]
